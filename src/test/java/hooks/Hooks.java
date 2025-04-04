3a1bfd9590ad347f88cd12ad3c57aa497288f6c1
alexmeunier <bobbymeunier@gmail.com>
2025-04-04 14:34:21 +0100

    Implementation of screenshot on failed step on Allure report

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -3,11 +3,21 @@ package hooks;
 import constants.FrameworkConstants;
 import driver.DriverFactory;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utils.PropertyUtils;
 import utils.TestDataLoader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Hooks {
@@ -25,6 +35,7 @@ public class Hooks {
         String headlessMode = properties.getProperty("headless");
 
         driver	= DriverFactory.initDriver(FrameworkConstants.BROWSER, FrameworkConstants.DEVICE, headlessMode);
+        logBrowserAndViewportDetails(FrameworkConstants.BROWSER, FrameworkConstants.DEVICE);
         driver.get(TestDataLoader.getInstance().getAppURL());
 
     }
@@ -36,6 +47,58 @@ public class Hooks {
             driver.quit();
             driver = null;
         }
+    }
+
+    @AfterStep
+    public void afterStep(Scenario scenario) {
+        if (scenario.isFailed()) {
+            // Capture the screenshot if the step failed
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                // Save screenshot to desired location
+                File screenshotLocation = new File("target/screenshots/" + scenario.getName() + "_failure.png");
+                FileUtils.copyFile(screenshot, screenshotLocation);
+                // Attach the screenshot to Allure
+                attachScreenshotToAllure(screenshotLocation);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Method to attach screenshot to Allure report
+    private void attachScreenshotToAllure(File screenshot) {
+        try (FileInputStream screenshotInputStream = new FileInputStream(screenshot)) {
+            // Add screenshot attachment to Allure
+            Allure.addAttachment("Screenshot", "image/png", screenshotInputStream, ".png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Step("Browser: {0}, Viewport: {1}")
+    public static void logBrowserAndViewportDetails(String browser, String device) {
+        // Log the browser type and viewport size to Allure
+        Allure.addAttachment("Browser", browser);
+        Allure.addAttachment("Viewport", getViewportDimensions(device));
+    }
+
+    private static String getViewportDimensions(String device) {
+        String viewport = "";
+        switch (device.toLowerCase()) {
+            case "desktop":
+                viewport = FrameworkConstants.DESKTOP_WIDTH + "x" + FrameworkConstants.DESKTOP_HEIGHT;
+                break;
+            case "tablet":
+                viewport = FrameworkConstants.TABLET_WIDTH + "x" + FrameworkConstants.TABLET_HEIGHT;
+                break;
+            case "mobile":
+                viewport = FrameworkConstants.MOBILE_WIDTH + "x" + FrameworkConstants.MOBILE_HEIGHT;
+                break;
+            default:
+                throw new IllegalStateException("Invalid device specified");
+        }
+        return viewport;
     }
 
 
--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/UserJourney.feature",
+@CucumberOptions(features = "src/test/resources/features",
 glue = {"stepdefinitions", "hooks"},
 plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 public class TestRunner {
